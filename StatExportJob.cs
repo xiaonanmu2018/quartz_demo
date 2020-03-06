748fb8eefb7571bffe6e417d97ea6932bf608aeb
xiaonanmu2018 <43690991+xiaonanmu2018@users.noreply.github.com>
2020-03-06 14:16:19 +0800

    Add files via upload

---
+++ StatExportJob.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace DC.Web
+{
+    public class StatExportJob : IJob
+    {
+        private readonly ILogger<StatExportJob> _logger; 
+        //也可以注入项目中其他的实例    
+        public StatExportJob(ILogger<StatExportJob> logger)
+        {
+            _logger = logger;
+            _jobsService = jobsService;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Task.Yield();
+
+           _logger.LogInformation("test");  
+        }
+    }
+}
